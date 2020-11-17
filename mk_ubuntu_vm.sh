93deb5c6c54307ddac23f57c8a9e4dd3ae04e220
Mick Miller <mick@mickmill.com>
2020-11-17 18:24:37 +0000

    updated mk_ubuntu_vm.sh to allow vm name on cmd line

--- mk_ubuntu_vm.sh
+++ mk_ubuntu_vm.sh
@@ -1,19 +1,49 @@
+#! /bin/bash
+#===================================================================================
+# Name: mk_ubunut_vm.sh
+# Created by: Mick Miller
+# Created on: 2020-11-17
+#===================================================================================
+# set -e
+clear
+
+
+# Read command line args
+if [[ "${#}" -ne 1 ]]; then
+  echo 
+  echo ===========================================================================
+  echo Usage: "${0}" the pass in a name for you VM on the command line.
+  echo ===========================================================================
+  echo
+  exit 1
+fi
+
+KVM_NAME="${1}"
+
+
+make_vm() {
+  echo 
+  echo ===========================================================================
+  echo Building VM... "${1}"
+  echo ===========================================================================
+  echo
+
 virt-install \
---name vault-1 \
---ram 4096 \
---disk path=/data-1/vm-images/vault-1.img,size=50 \
---vcpus 2 \
---virt-type kvm \
---os-type linux \
---os-variant ubuntu18.04 \
---graphics none \
---console pty,target_type=serial \
---location 'http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/' \
---extra-args "console=tty0 console=ttyS0,115200n8"
-<<<<<<< HEAD
---network bridge=br0 \
---extra-args "ip=10.1.1.30::10.1.1.1:255.255.255.0:ipa.srv.01.tmcg.local:eth0=none"
-=======
---network bridge=virbr0 \
---extra-args "ip=10.1.1.30::10.1.1.1:255.255.255.0:unifi-srv-01.tmcg.local:eth0=none"
->>>>>>> 2e1a916d9ff76c99fe52e9563dd1c704bb4992c9
+  --name "${KVM_NAME}" \
+  --ram 4096 \
+  --disk path=/data-1/vm-images/"${KVM_NAME}".img,size=50 \
+  --vcpus 2 \
+  --virt-type kvm \
+  --os-type linux \
+  --os-variant ubuntu20.04 \
+  --graphics none \
+  --console pty,target_type=serial \
+  --location 'http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/' \
+  --extra-args 'console=tty0 console=ttyS0,115200n8' \
+  --network bridge=br0 \
+  --extra-args 'ip=10.1.1.60::10.1.1.1:255.255.255.0:vault-01.tmcg.local'
+}
+
+
+# main
+make_vm
--- mk_vault_vm.sh
+++ mk_vault_vm.sh
@@ -1,16 +1,15 @@
 virt-install \
---name vault-01 \
+--name vault-1 \
 --ram 4096 \
---disk path=/data-1/vm-images/vault-01.img,size=50 \
+--disk path=/data-1/vm-images/vault-1.img,size=50 \
 --vcpus 2 \
 --virt-type kvm \
 --os-type linux \
 --os-variant ubuntu20.04 \
 --graphics none \
 --console pty,target_type=serial \
---location '/data-1/vm-images/iso/ubuntu-20.04-live-server-amd64.iso' \
 --location 'http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/' \
 --extra-args "console=tty0 console=ttyS0,115200n8"
 --network bridge=br0 \
---extra-args "ip=10.1.1.30::10.1.1.1:255.255.255.0:vault-01.tmcg.local:eth0=none"
+--extra-args "ip=10.1.1.60::10.1.1.1:255.255.255.0:vault-01.tmcg.local"
 
